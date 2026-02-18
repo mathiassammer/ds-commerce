c5a9afa5d32550edaef0c701f9e60857ff8e0f6f
Mathias Sammer <mathias.sammer.dev@gmail.com>
2026-02-18 09:03:48 -0300

    feat: implemented GET Products paged

--- src/main/java/com/mathiassammer/dscommerce/controllers/ProductController.java
+++ src/main/java/com/mathiassammer/dscommerce/controllers/ProductController.java
@@ -1,16 +1,14 @@
 package com.mathiassammer.dscommerce.controllers;
 
 import com.mathiassammer.dscommerce.dto.ProductDTO;
-import com.mathiassammer.dscommerce.entities.Product;
-import com.mathiassammer.dscommerce.repositories.ProductRepository;
 import com.mathiassammer.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -22,5 +20,10 @@ public class ProductController {
     @GetMapping("/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return productService.findById(id);
+    }
+
+    @GetMapping()
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return productService.findAll(pageable);
     }
 }
