d1d86f729e7812811ca27f2d9d7f6f21784865dc
Mathias Sammer <mathias.sammer.dev@gmail.com>
2026-02-21 22:38:06 -0300

    feat: Add Bean Validation

--- src/main/java/com/mathiassammer/dscommerce/controllers/ProductController.java
+++ src/main/java/com/mathiassammer/dscommerce/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.mathiassammer.dscommerce.controllers;
 
 import com.mathiassammer.dscommerce.dto.ProductDTO;
 import com.mathiassammer.dscommerce.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -31,14 +32,14 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+    public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
         dto = productService.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
         dto = productService.update(id, dto);
         return ResponseEntity.ok(dto);
     }
--- src/main/java/com/mathiassammer/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/mathiassammer/dscommerce/dto/ProductDTO.java
@@ -2,12 +2,18 @@ package com.mathiassammer.dscommerce.dto;
 
 import com.mathiassammer.dscommerce.entities.Product;
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
 
 public class ProductDTO {
 
     private Long id;
+    @Size(min = 3, max = 80, message = "Nome precisa ter entre 3 e 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size(min = 10, message = "Descrição precisa ter no mínimo caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
